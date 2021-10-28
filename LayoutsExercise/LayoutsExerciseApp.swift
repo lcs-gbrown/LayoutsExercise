5c04cd72801d4b598d749c7c10ef8ac3982ea791
gabi brown <gbrown@lcs.on.ca>
2021-10-28 13:28:56 -0400

    Initial Commit

---
+++ LayoutsExercise/LayoutsExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsExerciseApp.swift
+//  LayoutsExercise
+//
+//  Created by gabi brown on 2021-10-28.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
