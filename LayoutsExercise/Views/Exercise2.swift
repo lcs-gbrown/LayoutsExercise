8e27bd6787f71d963ac9365186f0e094ec33a95e
gabi brown <gbrown@lcs.on.ca>
2021-10-28 20:00:40 -0400

    Made second view

---
+++ LayoutsExercise/Views/Exercise2.swift
@@ -0,0 +1,30 @@
+//
+//  Exercise2.swift
+//  LayoutsExercise
+//
+//  Created by gabi brown on 2021-10-28.
+//
+
+import SwiftUI
+
+struct Exercise2: View {
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: "https://placekitten.com/300/200"))
+                .frame(width: 300, height: 200)
+            HStack {
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+            }
+        }
+    }
+}
+
+struct Exercise2_Previews: PreviewProvider {
+    static var previews: some View {
+        Exercise2()
+    }
+}
